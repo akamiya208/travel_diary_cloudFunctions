43e19ff6434f29d9b21be532b73fcc1d7c3af438
akamiya208 <a.kamiya.208@gmail.com>
2020-09-04 12:50:35 +0900

    update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
  * @param {!express:Response} res HTTP response context.
  */
 const select_image = require('./select_image');
-const { load_img, concat_img, make_text_img } = require('./image_processing');
-const { uploadToGCS, getPublicUrl } = require('./gcs');
+const image_processing = require('./image_processing');
+const gcs = require('./gcs');
 
 
 exports.sendImageApi = (req, res) => {
@@ -16,17 +16,17 @@ exports.sendImageApi = (req, res) => {
 
     const img_paths = await select_image(text); // TODO: select_imageの引数は変更要
 
-    const promises = [load_img(img_paths), make_text_img(text)];
+    const promises = [image_processing.load_img(img_paths), image_processing.make_text_img(text)];
     const result = await Promise.all(promises).then((results) => { return results });
-    const diary_img = await concat_img(result[0], result[1]);
+    const diary_img = await image_processing.concat_img(result[0], result[1]);
         
     const filename = Date.now() + ".png"
     const file_path = process.env['ENV'] +'/diary/' + filename;
     
-    await uploadToGCS(diary_img, file_path)
+    await gcs.uploadToGCS(diary_img, file_path)
           .then( () => {
             res.status(200).json({
-              "url": getPublicUrl(file_path)
+              "url": gcs.getPublicUrl(file_path)
             });
           })
           .catch( (err) => {
